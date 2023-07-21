ff4fca074f0cd68de1babb5ad5226c5da2f18bce
MaksymMoskalov <max.godnavi133@gmail.com>
2023-07-21 03:46:32 +0300

    Home done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector(".gallery");
+const renderedGalleryItems = createGalleryItem(galleryItems);
+galleryListEl.insertAdjacentHTML("beforeend", renderedGalleryItems);
+
+galleryListEl.addEventListener("click", onGalleryItemClick);
+
+function createGalleryItem(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+</li>`;
+    })
+    .join("");
+}
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+  const currentClick = evt.target;
+  if (!currentClick.classList.contains("gallery__image")) {
+    return;
+  }
+  const originalImage = currentClick.dataset.source;
+
+  const imageModal = basicLightbox.create(`
+      <img src="${originalImage}" >
+  `);
+
+  imageModal.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryListEl = document.querySelector(".gallery");
+const renderedGalleryItems = createGalleryItem(galleryItems);
+galleryListEl.insertAdjacentHTML("beforeend", renderedGalleryItems);
+galleryListEl.addEventListener("click", onGalleryItemClick);
 
-console.log(galleryItems);
+function createGalleryItem(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join("");
+}
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+  const currentClick = evt.target;
+  if (!currentClick.classList.contains("gallery__image")) {
+    return;
+  }
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
